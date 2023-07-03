6bf1b35d22cdd0b33d42b1cc23a1bac7faa2a672
nitin-novostack <nitin.kumar@novostack.com>
2023-07-03 17:05:29 +0530

    first commit

---
+++ assests/js/main.js
@@ -0,0 +1,14 @@
+
+// patnert swiper js start
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    autoplay:false,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    }
+});
+
+// partner-swiper js end 
+
