114679f6986569a5c0128bb9c4b20a14a1ba6278
nitin-novostack <nitin.kumar@novostack.com>
2023-07-21 11:01:57 +0530

    add custom toast

--- assests/js/main.js
+++ assests/js/main.js
@@ -18,3 +18,26 @@ function auto_grow(element) {
   element.style.height = "5px";
   element.style.height = (element.scrollHeight) + "px";
 }
+
+
+// 
+function auto_grow(element) {
+  element.style.height = "5px";
+  element.style.height = (element.scrollHeight) + "px";
+}
+
+function checkAndShowPopup() {
+  var emailInput = document.querySelector('input[type="email"]');
+  var queryInput = document.querySelector('textarea');
+
+  if (emailInput.value.trim() !== '' || queryInput.value.trim() !== '') {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'block';
+
+    setTimeout(function () {
+      popup.style.display = 'none';
+      emailInput.value = '';
+      queryInput.value = ''; 
+    }, 3000); 
+  }
+}
